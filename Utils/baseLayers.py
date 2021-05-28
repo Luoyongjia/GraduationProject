d7c67ba02f7c6cdea3c342e0d399ea933fc371c6
Luoyongjia <luoyongjiavb@gmail.com>
2021-05-28 21:48:42 +0800

    supernet model ok!

--- Utils/baseLayers.py
+++ Utils/baseLayers.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 
 
 class Conv2D(nn.Module):
@@ -28,9 +29,9 @@ class MaxPooling2D(nn.Module):
         return self.maxpool(x)
 
 
-class deConv2D(nn.Module):
+class convTranspose2D(nn.Module):
     def __init__(self, inChannels, outChannels, activation='lRelu'):
-        super(deConv2D, self).__init__()
+        super(convTranspose2D, self).__init__()
         self.activation = nn.LeakyReLU(inplace=True)
         if activation == 'Relu':
             self.activation = nn.ReLU(inplace=True)
@@ -53,3 +54,82 @@ class Concat(nn.Module):
         return torch.cat((x, y), dim=1)
 
 
+class BlurPool(nn.Module):
+    """
+    https://github.com/adobe/antialiased-cnns
+    proposed pooling method
+    ./antialiased_cnns/blurpool.py
+    """
+    def __init__(self, channels, pad_type='reflect', filt_size=4, stride=2, pad_off=0):
+        super(BlurPool, self).__init__()
+        self.filt_size = filt_size
+        self.pad_off = pad_off
+        self.pad_sizes = [int(1.*(filt_size-1)/2), int(np.ceil(1.*(filt_size-1)/2)), int(1.*(filt_size-1)/2), int(np.ceil(1.*(filt_size-1)/2))]
+        self.pad_sizes = [pad_size+pad_off for pad_size in self.pad_sizes]
+        self.stride = stride
+        self.off = int((self.stride-1)/2.)
+        self.channels = channels
+
+        # define the blur kernel
+        if self.filt_size == 1:
+            a = np.array([1., ])
+        elif self.filt_size == 2:
+            a = np.array([1., 1.])
+        elif self.filt_size == 3:
+            a = np.array([1., 2., 1.])
+        elif self.filt_size == 4:
+            a = np.array([1., 3., 3., 1.])
+        elif self.filt_size == 5:
+            a = np.array([1., 4., 6., 4., 1.])
+        elif self.filt_size == 6:
+            a = np.array([1., 5., 10., 10., 5., 1.])
+        elif self.filt_size == 7:
+            a = np.array([1., 6., 15., 20., 15., 6., 1.])
+
+        filt = torch.Tensor(a[:, None]*a[None, :])
+        filt = filt/torch.sum(filt)
+        self.register_buffer('filt', filt[None, None, :, :].repeat((self.channels, 1, 1, 1)))
+
+        self.pad = self.get_pad_layer(pad_type)(self.pad_sizes)
+
+    def forward(self, inp):
+        if self.filt_size == 1:
+            if self.pad_off == 0:
+                return inp[:, :, ::self.stride, ::self.stride]
+            else:
+                return self.pad(inp)[:, :, ::self.stride, ::self.stride]
+        else:
+            return F.conv2d(self.pad(inp), self.filt, stride=self.stride, groups=inp.shape[1])
+
+    def get_pad_layer(self, pad_type):
+        if pad_type in ['refl', 'reflect']:
+            PadLayer = nn.ReflectionPad2d
+        elif pad_type in ['repl', 'replicate']:
+            PadLayer = nn.ReplicationPad2d
+        elif pad_type == 'zero':
+            PadLayer = nn.ZeroPad2d
+        else:
+            print('Pad type [%s] not recognized' % pad_type)
+
+        return PadLayer
+
+
+class ResidualDownSample(nn.Module):
+    def __init__(self, inChannels, outChannels, bias=False):
+        super(ResidualDownSample, self).__init__()
+        self.left = nn.Sequential(
+            nn.Conv2d(inChannels, inChannels, kernel_size=3, stride=1, padding=1, bias=bias),
+            nn.ReLU(),
+            BlurPool(channels=inChannels, filt_size=3, stride=2),
+            nn.Conv2d(inChannels, outChannels, kernel_size=1, stride=1, padding=0, bias=bias)
+        )
+        self.right = nn.Sequential(
+            BlurPool(channels=inChannels, filt_size=3, stride=2),
+            nn.Conv2d(inChannels, outChannels, kernel_size=1, stride=1, padding=0, bias=bias)
+        )
+
+    def forward(self, x):
+        left = self.left(x)
+        right = self.right(x)
+
+        return left + right
--- supernet/model.py
+++ supernet/model.py
@@ -1,54 +1,88 @@
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
-import numpy as np
+from blocks.block import ChoiceBlock
 from Utils.baseLayers import *
 
+
 class mNet(nn.Module):
-    def __init__(self, filters=32, activation='lrelu'):
-        super().__init__()
+    def __init__(self, choiceNum=8, filters=32, activation='lrelu'):
+        super(mNet, self).__init__()
+
+        self.choiceNum = choiceNum
         self.convInput = Conv2D(3, filters)
         # R
-        self.convR1 = Conv2D(filters, filters*2)
-        self.maxpoolR1 = MaxPooling2D()
-        self.convR2 = Conv2D(filters*2, filters*4)
-        self.maxpoolR2 = MaxPooling2D()
-        self.deConvR1 = deConv2D(filters*4, filters*2)
-        self.concatR1 = Concat()
-        self.convR3 = Conv2D(filters*4, filters*2)
-        self.deConvR2 = deConv2D(filters*2, filters)
-        self.concatR2 = Concat()
-        self.convR4 = Conv2D(filters*2, filters)
-        self.convR5 = nn.Conv2d(filters, 3, kernel_size=3, padding=1)
-        self.ROut = nn.Sigmoid()
 
-        # L
-        self.convI1 = Conv2D(filters, filters)
-        self.concatI1 = Concat()
-        self.convI2 = nn.Conv2d(filters*2, 1, kernel_size=3, padding=1)
-        self.Iout = nn.Sigmoid()
+        self.R_conv1 = Conv2D(filters, filters*2)
+        self.R_down1 = ResidualDownSample(filters*2, filters*2)
+        self.R_choice1 = self.buildChoiceBlock(filters*2, filters*4)
+        self.R_down2 = ResidualDownSample(filters*4, filters*4)
+        self.R_choice2 = self.buildChoiceBlock(filters*4, filters*8)
+        self.R_down3 = ResidualDownSample(filters*8, filters*8)
+        self.R_choice3 = self.buildChoiceBlock(filters*8, filters*16)
+        self.R_up1 = convTranspose2D(filters*16, filters*8)
+        self.concat1 = Concat()
+        self.R_choice4 = self.buildChoiceBlock(filters*16, filters*8)
+        self.R_up2 = convTranspose2D(filters*8, filters*4)
+        self.concat2 = Concat()
+        self.R_choice5 = self.buildChoiceBlock(filters*8, filters*4)
+        self.R_up3 = convTranspose2D(filters*4, filters*2)
+        self.concat3 = Concat()
+        self.R_choice6 = self.buildChoiceBlock(filters*4, filters*4)
+        self.R_choice7 = self.buildChoiceBlock(filters*4, filters*2)
+        self.R_choice8 = self.buildChoiceBlock(filters*2, filters)
+        self.R_sigmoid = nn.Sigmoid()
 
-    def forward(self, x):
+
+
+        # I
+        self.I_conv1 = Conv2D(filters, filters)
+        self.I_conv2 = Conv2D(filters, filters)
+        self.I_conv3 = Conv2D(filters, filters)
+        self.I_concat = Concat()
+        self.I_conv4 = Conv2D(filters*2, filters)
+        self.I_conv5 = Conv2D(filters, filters)
+        self.I_conv6 = nn.Conv2d(filters, 3, kernel_size=3, padding=1)
+        self.I_sigmoid = nn.Sigmoid()
+
+    def forward(self, x, arch):
         conv1 = self.convInput(x)
 
         # R
-        maxpoolR1 = self.maxpoolR1(conv1)
-        convR2 = self.convR1(maxpoolR1)
-        maxpoolR2 = self.maxpoolR2(convR2)
-        convR3 = self.convR2(maxpoolR2)
-        deConvR1 = self.deConvR1(convR3)
-        concatR1 = self.concatR1(deConvR1, convR2)
-        convR4 = self.convR3(concatR1)
-        deConvR2 = self.deConvR2(convR4)
-        concatR2 = self.concatR2(deConvR2, conv1)
-        convR5 = self.convR4(concatR2)
-        convR6 = self.convR5(convR5)
-        ROut = self.ROut(convR6)
+        R_conv1 = self.R_conv1(conv1)
+        R_down1 = self.R_down1(R_conv1)
+        R_chose1 = self.R_choice1[arch[0]](R_down1)
+        R_down2 = self.R_down2(R_chose1)
+        R_chose2 = self.R_choice2[arch[1]](R_down2)
+        R_down3 = self.R_down3(R_chose2)
+        R_chose3 = self.R_choice3[arch[2]](R_down3)
+        R_up1 = self.R_up1(R_chose3)
+        R_concat1 = self.concat1(R_chose2, R_up1)
+        R_chose4 = self.choice4[arch[3]](R_concat1)
+        R_up2 = self.R_up2(R_chose4)
+        R_concat2 = self.concat2(R_chose1, R_up2)
+        R_chose5 = self.choice5[arch[4]](R_concat2)
+        R_up3 = self.R_up3(R_chose5)
+        R_concat3 = self.concat2(R_conv1, R_up3)
+        R_chose6 = self.choice6[arch[5]](R_concat3)
+        R_chose7 = self.choice7[arch[6]](R_chose6)
+        R_chose8 = self.choice8[arch[7]](R_chose7)
+        R_out = self.R_sigmoid(R_chose8)
 
-        #I
-        convI1 = self.convI1(conv1)
-        concatI1 = self.concatI1(convI1, convR5)
-        convI2 = self.convI2(concatI1)
-        IOut = self.Iout(convI2)
+        # I
+        I_conv1 = self.I_conv1(conv1)
+        I_conv2 = self.I_conv2(I_conv1)
+        I_conv3 = self.I_conv3(I_conv2)
+        I_concat = self.I_concat(I_conv3, R_chose7)
+        I_conv4 = self.I_ocnv4(I_concat)
+        I_conv5 = self.I_conv5(I_conv4)
+        I_conv6 = self.I_conv6(I_conv5)
+        I_out = self.I_sigmoid(I_conv6)
 
-        return ROut, IOut
+        return R_out, I_out
+
+    def buildChoiceBlock(self, inChannels, outChannels):
+        choiceBlock = torch.nn.ModuleList()
+        for choice in range(self.choiceNum):
+            choiceBlock.append(ChoiceBlock(choice, inChannels, outChannels))
+
+        return choiceBlock
